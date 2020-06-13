88c7775cc9ec68c872c51d41c8ad550136a97bbf
higorhms <higorhmscebrac@hotmail.com>
2020-06-13 14:22:50 -0300

    Setting structure

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,32 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+
+class Server {
+  private server: Express;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.startServer();
+  }
+
+  middlewares(): void {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  routes(): void {
+    this.server.use(routes);
+  }
+
+  startServer(): void {
+    this.server.listen(3333);
+    console.log('Server started on port 3333');
+  }
+}
+
+export default new Server();
