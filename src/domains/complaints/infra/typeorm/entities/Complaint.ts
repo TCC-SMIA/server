5ce30a922af6d7a7816c9634783bbf9f6581e88c
Higor <higorhmscebrac@hotmail.com>
2020-07-22 09:16:35 -0300

    add complaint entity

---
+++ src/domains/complaints/infra/typeorm/entities/Complaint.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from '@domains/users/infra/typeorm/entities/User';
+
+@Entity('complaints')
+class Complaint {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  content: Date;
+
+  @Column('time with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Complaint;
