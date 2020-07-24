83b0db872bee60b73b2dbb310fd81ae971bc5512
Higor <higorhmscebrac@hotmail.com>
2020-07-23 21:31:04 -0300

    refactor: agency dependency

--- src/domains/users/container/index.ts
+++ src/domains/users/container/index.ts
@@ -4,10 +4,17 @@ import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
 import IUsersRepository from '../rules/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/rules/IHashProvider';
 import BCryptHashProvider from '../providers/HashProvider/implementations/BCryptHashProvider';
+import AgencyRepository from '../infra/typeorm/repositories/AgencyRepository';
+import IAgencyRepository from '../rules/IAgencyRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IAgencyRepository>(
+  'AgencyRepository',
+  AgencyRepository,
 );
 
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
