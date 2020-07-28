1568ddc5441afdf2e98e96705eadb1e4732aa2b2
Higor <higorhmscebrac@hotmail.com>
2020-07-28 16:44:47 -0300

    feature: add multer config

---
+++ src/config/multer.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  tmpFolder,
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(_, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+};
