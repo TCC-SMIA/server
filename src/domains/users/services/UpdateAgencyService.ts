d972455a9da1877b4402405f0c3e713c478bbc3c
gabrielrportugal <gabrielrportugal@outlook.com>
2020-07-29 19:52:47 -0300

    Creating tests for UpdateAgencyService

--- src/domains/users/services/UpdateAgencyService.ts
+++ src/domains/users/services/UpdateAgencyService.ts
@@ -8,11 +8,10 @@ import Agency from '../infra/typeorm/entities/Agency';
 interface IRequest {
   agencyId: string;
   name: string;
-  cnpj: string;
   email: string;
   oldpassword?: string;
   password?: string;
-  password_confirmation: string;
+  password_confirmation?: string;
 }
 
 @injectable()
@@ -20,6 +19,7 @@ class UpdateAgencyService {
   constructor(
     @inject('AgencyRepository')
     private agencyRepository: IAgencyRepository,
+
     @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
@@ -27,13 +27,13 @@ class UpdateAgencyService {
   async execute({
     agencyId,
     name,
-    cnpj,
     email,
     oldpassword,
     password,
     password_confirmation,
   }: IRequest): Promise<Agency> {
     const agency = await this.agencyRepository.findById(agencyId);
+
     if (!agency) {
       throw new AppError('Agency not found');
     }
@@ -58,9 +58,11 @@ class UpdateAgencyService {
         oldpassword,
         agency.password,
       );
+
       if (!passwordMatched) {
-        throw new AppError('old password is Wrong');
+        throw new AppError('old password is wrong');
       }
+
       if (password !== password_confirmation) {
         throw new AppError(
           'Password and password_confirmation must be the same',
