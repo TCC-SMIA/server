19aa950b5532eeeb7d08b496bb27389c0f31d8af
gabrielrportugal <gabrielrportugal@outlook.com>
2020-07-28 22:17:15 -0300

    Creating routes and controller for Password and finishing UserTokensRepository

--- src/domains/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ src/domains/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -10,9 +10,23 @@ class UserTokensRepository implements IUserTokensRepository {
     this.userTokensRepository = getRepository(UserToken);
   }
 
-  public async generate(user_id: string): Promise<UserToken> {}
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.userTokensRepository.create({
+      user_id,
+    });
 
-  public async findByToken(token: string): Promise<UserToken | undefined> {}
+    await this.userTokensRepository.save(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.userTokensRepository.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
 }
 
 export default UserTokensRepository;
