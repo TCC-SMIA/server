509205f84081fc6a63cc3a4959ac7b153a0797c8
gabrielrportugal <gabrielrportugal@outlook.com>
2020-07-28 22:49:37 -0300

    Implementing EtherealMailProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,2 +1,12 @@
+import { container } from 'tsyringe';
+
 import '@domains/users/container';
 import '@domains/complaints/container';
+
+import IMailProvider from '@shared/providers/MailProvider/rules/IMailProvider';
+import EtherealMailProvider from '@shared/providers/MailProvider/implementations/EtherealMailProvider';
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
+);
