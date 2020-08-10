399395037703c0421a0b6b5a1c870ba794d1ffc8
Higor <higorhmscebrac@hotmail.com>
2020-08-10 09:40:33 -0300

    tests: ensure CreateCommentService creates a new comment

--- src/__tests__/users/services/CreateCommentService.spec.ts
+++ src/__tests__/users/services/CreateCommentService.spec.ts
@@ -3,6 +3,9 @@ import CreateCommentService from '@domains/complaints/services/CreateCommentServ
 import ICommentsRepository from '@domains/complaints/rules/ICommentsRepository';
 import IUsersRepository from '@domains/users/rules/IUsersRepository';
 import IComplaintsRepository from '@domains/complaints/rules/IComplaintsRepository';
+import FakeComplaintsRepository from '@tests/complaints/fakes/FakeComplaintsRepository';
+import FakeUsersRepository from '../fakes/FakeUsersRepository';
+import FakeCommentsRepository from '../fakes/FakeCommentsRepository';
 
 let commentRepository: ICommentsRepository;
 let usersRepository: IUsersRepository;
@@ -11,6 +14,9 @@ let createCommentService: CreateCommentService;
 
 describe('CreateCommentService', () => {
   beforeEach(() => {
+    usersRepository = new FakeUsersRepository();
+    complaintRepository = new FakeComplaintsRepository();
+    commentRepository = new FakeCommentsRepository();
     createCommentService = new CreateCommentService(
       commentRepository,
       usersRepository,
@@ -19,6 +25,33 @@ describe('CreateCommentService', () => {
   });
 
   it('should be able to create a new comment', async () => {
-    expect(1 + 1).toBe(2);
+    const user = await usersRepository.create({
+      name: 'jhon',
+      email: 'doe@doe.com',
+      nickname: 'johnzins',
+      password: '123123',
+    });
+
+    const complaint = await complaintRepository.create({
+      title: 'Baleia encalhada',
+      description:
+        'Encontramos uma baleia encalhada na praia do forte, Cabo Frio',
+      latitude: -22.88248,
+      longitude: -42.0737652,
+      anonymous: false,
+      date: new Date(),
+    });
+
+    const comment = await createCommentService.execute({
+      user_id: user.id,
+      complaint_id: complaint.id,
+      content: 'New comment',
+    });
+
+    expect(comment).toBeTruthy();
+    expect(comment.id).toBeTruthy();
+    expect(comment.user.id).toBe(user.id);
+    expect(comment.complaint.id).toBe(complaint.id);
+    expect(comment.content).toBe('New comment');
   });
 });
