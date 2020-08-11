27383b36845335683277efd9c7af501cac320f2f
Higor <higorhmscebrac@hotmail.com>
2020-08-11 09:24:09 -0300

    refactor: Notification entity

--- src/domains/notifications/infra/typeorm/entities/Notification.ts
+++ src/domains/notifications/infra/typeorm/entities/Notification.ts
@@ -4,27 +4,34 @@ import {
   UpdateDateColumn,
   CreateDateColumn,
   PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import User from '@domains/users/infra/typeorm/entities/User';
 
 @Entity('notifications')
 class Notification {
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  id!: string;
 
   @Column()
-  content: string;
+  content!: string;
 
-  @Column('uuid')
-  user_id: string;
+  @Column()
+  user_id!: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
 
   @Column({ default: false })
-  read: boolean;
+  read!: boolean;
 
   @CreateDateColumn()
-  created_at: Date;
+  created_at!: Date;
 
   @UpdateDateColumn()
-  updated_at: Date;
+  updated_at!: Date;
 }
 
 export default Notification;
--- src/domains/notifications/rules/INotificationsRepository.ts
+++ src/domains/notifications/rules/INotificationsRepository.ts
@@ -1,5 +1,4 @@
 import ICreateNotificationDTO from '../dtos/ICreateNotificationsDTO';
-import Notification from '../infra/typeorm/schemas/Notification';
 
 export default interface INotificationsRepository {
   create(data: ICreateNotificationDTO): Promise<Notification>;
