4a660dafb977926b3564b65fa5db55c4f93d90bf
gabrielrportugal <gabrielrportugal@outlook.com>
2020-08-11 19:29:04 -0300

    feature: creating diskStorage rules and implementation

--- src/domains/users/infra/http/controllers/AvatarController.ts
+++ src/domains/users/infra/http/controllers/AvatarController.ts
@@ -1,8 +1,21 @@
 import { Response, Request } from 'express';
 
+import UpdateUserAvatarService from '@domains/users/services/UpdateUserAvatarService';
+import { container } from 'tsyringe';
+
 class AvatarController {
   async update(request: Request, response: Response): Promise<Response> {
-    console.log(request.file);
+    const user_id = request.user.id;
+
+    const { filename } = request.file;
+
+    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
+
+    await updateUserAvatar.execute({
+      user_id,
+      avatarFilename: filename,
+    });
+
     return response.json({ success: true });
   }
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,7 +7,15 @@ import '@domains/notifications/container';
 import IMailProvider from '@shared/providers/MailProvider/rules/IMailProvider';
 import EtherealMailProvider from '@shared/providers/MailProvider/implementations/EtherealMailProvider';
 
+import IStorageProvider from '@shared/providers/StorageProvider/rules/IStorageProvider';
+import DiskStorageProvider from '@shared/providers/StorageProvider/implementations/DiskStorageProvider';
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  DiskStorageProvider,
 );
