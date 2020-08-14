884acd49c70eea2e290d1f945aea17eafd5565b1
Higor <higorhmscebrac@hotmail.com>
2020-08-13 21:49:10 -0300

    refactor: notification creation

--- src/domains/notifications/container/index.ts
+++ src/domains/notifications/container/index.ts
@@ -2,8 +2,14 @@ import { container } from 'tsyringe';
 
 import INotificationsRepository from '../rules/INotificationsRepository';
 import NotificationsRepository from '../infra/typeorm/repositories/NotificationsRepository';
+import CreateNotificationService from '../services/CreateNotificationService';
 
 container.registerSingleton<INotificationsRepository>(
   'NotificationsRepository',
   NotificationsRepository,
+);
+
+container.registerSingleton<CreateNotificationService>(
+  'CreateNotificationService',
+  CreateNotificationService,
 );
