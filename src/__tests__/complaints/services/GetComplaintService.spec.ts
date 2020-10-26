ed4ac0a4d62e4a3f252236e10c08af7eac3a7e0a
Higor <higorhmscebrac@hotmail.com>
2020-10-26 20:11:58 -0300

    test: ensure GetComplaintService not returns a complaint it it does not exists

--- src/__tests__/complaints/services/GetComplaintService.spec.ts
+++ src/__tests__/complaints/services/GetComplaintService.spec.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import IComplaintsRepository from '@domains/complaints/rules/IComplaintsRepository';
 import GetComplaintService from '@domains/complaints/services/GetComplaintService';
 import Complaint from '@domains/complaints/infra/typeorm/entities/Complaint';
+import AppError from '@shared/errors/AppError';
 import FakeComplaintsRepository from '../fakes/FakeComplaintsRepository';
 
 let fakeComplaintsRepository: IComplaintsRepository;
@@ -25,5 +26,13 @@ describe('GetComplaintService', () => {
 
     expect(complaint).toBeTruthy();
     expect(complaint.id).toBeTruthy();
+  });
+
+  it('Should not be able to return a complaint if complaint does not exists', async () => {
+    await expect(
+      getComplaintService.execute({
+        complaint_id: 'valid_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
