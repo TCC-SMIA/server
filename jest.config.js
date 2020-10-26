4280ce34acd2a66c22ad0878d55e903725abb1c2
Higor <higorhmscebrac@hotmail.com>
2020-10-26 19:56:39 -0300

    chore: decrease jest threshold

--- jest.config.js
+++ jest.config.js
@@ -19,10 +19,10 @@ module.exports = {
     ],
     coverageThreshold: {
         global: {
-            statements: 100,
-            branches: 100,
-            functions: 100,
-            lines: 100,
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90,
         },
     },
 };
