1658b24266626114fe17e3c24a7b99b87f0a5a80
Higor <higorhmscebrac@hotmail.com>
2020-11-14 16:36:05 -0300

    test: ensure GetChatsByUserService returns a list of chats

---
+++ src/__tests__/chats/services/GetChatsByUserService.spec.ts
@@ -0,0 +1,41 @@
+import IChatsRepository from '@domains/chats/rules/IChatsRepository';
+import GetChatsByUserService from '@domains/chats/services/GetChatsByUserService';
+import IUsersRepository from '@domains/users/rules/IUsersRepository';
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@tests/users/fakes/FakeUsersRepository';
+import FakeChatsRepository from '../fakes/FakeChatsRepository';
+
+let fakeChatsRepository: IChatsRepository;
+let fakeUsersRepository: IUsersRepository;
+let service: GetChatsByUserService;
+
+describe('GetChatsByUserService', () => {
+  beforeAll(() => {
+    fakeChatsRepository = new FakeChatsRepository();
+    fakeUsersRepository = new FakeUsersRepository();
+    service = new GetChatsByUserService(
+      fakeChatsRepository,
+      fakeUsersRepository,
+    );
+  });
+
+  it('Should be able to get the user chat', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'doe@doe.com',
+      nickname: 'johnzins',
+      password: '123123',
+    });
+
+    const chats = await service.execute({ user_id: user.id });
+
+    expect(chats).toBeTruthy();
+    expect(chats).toBeInstanceOf(Array);
+  });
+
+  it('Should not be able to get the list of chats if user does not exists', async () => {
+    await expect(
+      service.execute({ user_id: 'invalid_id' }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
